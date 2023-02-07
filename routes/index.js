1cc40f5bfd6e014a8015720d7d459e9fb51eb821
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-02-06 19:28:54 -0500

    Added save notes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
+const apiRouter = require('./apiRoutes');
 const feedbackRouter = require('./feedback');
 
 const app = express();
 
-app.use('/notes', tipsRouter);
+app.use('/notes', apiRouter);
 app.use('/feedback', feedbackRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', api);
 
-app.use(express.static('public'));
+
 
 // GET Route for homepage
 app.get('/notes', (req, res) =>
